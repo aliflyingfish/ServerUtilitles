d5970a1dcf6f6aab4e99c348bc3f56ea540393e6
aliflyingfish <63774174+aliflyingfish@users.noreply.github.com>
2020-12-25 19:15:03 +0800

    20201225

--- WindowsConsole/Form1.Designer.cs
+++ WindowsConsole/Form1.Designer.cs
@@ -28,11 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SplineAreaSeriesView splineAreaSeriesView1 = new DevExpress.XtraCharts.SplineAreaSeriesView();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.radStatusStrip1 = new Telerik.WinControls.UI.RadStatusStrip();
             this.statusText = new Telerik.WinControls.UI.RadLabelElement();
             this.fluentDarkTheme1 = new Telerik.WinControls.Themes.FluentDarkTheme();
+            this.memoEdit1 = new DevExpress.XtraEditors.MemoEdit();
+            this.splitContainerControl1 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.splitContainerControl2 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.splitContainerControl3 = new DevExpress.XtraEditors.SplitContainerControl();
+            this.groupControl1 = new DevExpress.XtraEditors.GroupControl();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
             ((System.ComponentModel.ISupportInitialize)(this.radStatusStrip1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).BeginInit();
+            this.splitContainerControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl2)).BeginInit();
+            this.splitContainerControl2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(splineAreaSeriesView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl3)).BeginInit();
+            this.splitContainerControl3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // radStatusStrip1
@@ -47,12 +71,124 @@
             // 
             // statusText
             // 
+            this.statusText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
             this.statusText.Font = new System.Drawing.Font("微软雅黑", 9F);
             this.statusText.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
             this.statusText.Name = "statusText";
+            this.statusText.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
             this.radStatusStrip1.SetSpring(this.statusText, true);
             this.statusText.Text = "就绪";
             this.statusText.TextWrap = true;
+            // 
+            // memoEdit1
+            // 
+            this.memoEdit1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.memoEdit1.EditValue = "Server Auto Bash";
+            this.memoEdit1.Location = new System.Drawing.Point(0, 0);
+            this.memoEdit1.Name = "memoEdit1";
+            this.memoEdit1.Properties.Appearance.Font = new System.Drawing.Font("Consolas", 10.5F);
+            this.memoEdit1.Properties.Appearance.Options.UseFont = true;
+            this.memoEdit1.Size = new System.Drawing.Size(426, 612);
+            this.memoEdit1.TabIndex = 2;
+            // 
+            // splitContainerControl1
+            // 
+            this.splitContainerControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl1.Name = "splitContainerControl1";
+            this.splitContainerControl1.Panel1.Controls.Add(this.memoEdit1);
+            this.splitContainerControl1.Panel1.Text = "Panel1";
+            this.splitContainerControl1.Panel2.Controls.Add(this.splitContainerControl2);
+            this.splitContainerControl1.Panel2.Text = "Panel2";
+            this.splitContainerControl1.Size = new System.Drawing.Size(917, 612);
+            this.splitContainerControl1.SplitterPosition = 426;
+            this.splitContainerControl1.TabIndex = 3;
+            // 
+            // splitContainerControl2
+            // 
+            this.splitContainerControl2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl2.Horizontal = false;
+            this.splitContainerControl2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl2.Name = "splitContainerControl2";
+            this.splitContainerControl2.Panel1.Controls.Add(this.chartControl1);
+            this.splitContainerControl2.Panel1.Text = "Panel1";
+            this.splitContainerControl2.Panel2.Controls.Add(this.splitContainerControl3);
+            this.splitContainerControl2.Panel2.Text = "Panel2";
+            this.splitContainerControl2.Size = new System.Drawing.Size(481, 612);
+            this.splitContainerControl2.SplitterPosition = 260;
+            this.splitContainerControl2.TabIndex = 0;
+            // 
+            // chartControl1
+            // 
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisX.VisualRange.Auto = false;
+            xyDiagram1.AxisX.VisualRange.EndSideMargin = 0D;
+            xyDiagram1.AxisX.VisualRange.MaxValueSerializable = "240";
+            xyDiagram1.AxisX.VisualRange.MinValueSerializable = "0";
+            xyDiagram1.AxisX.VisualRange.StartSideMargin = 0D;
+            xyDiagram1.AxisX.WholeRange.Auto = false;
+            xyDiagram1.AxisX.WholeRange.EndSideMargin = 0D;
+            xyDiagram1.AxisX.WholeRange.MaxValueSerializable = "240";
+            xyDiagram1.AxisX.WholeRange.MinValueSerializable = "0";
+            xyDiagram1.AxisX.WholeRange.StartSideMargin = 0D;
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.Legend.Name = "Default Legend";
+            this.chartControl1.Location = new System.Drawing.Point(0, 0);
+            this.chartControl1.Name = "chartControl1";
+            series1.Name = "Java";
+            splineAreaSeriesView1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(0)))), ((int)(((byte)(176)))), ((int)(((byte)(80)))));
+            splineAreaSeriesView1.Transparency = ((byte)(205));
+            series1.View = splineAreaSeriesView1;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.Size = new System.Drawing.Size(481, 260);
+            this.chartControl1.TabIndex = 0;
+            // 
+            // splitContainerControl3
+            // 
+            this.splitContainerControl3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainerControl3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainerControl3.Name = "splitContainerControl3";
+            this.splitContainerControl3.Panel1.Controls.Add(this.groupControl1);
+            this.splitContainerControl3.Panel1.Text = "Panel1";
+            this.splitContainerControl3.Panel2.Controls.Add(this.simpleButton1);
+            this.splitContainerControl3.Panel2.Controls.Add(this.textEdit1);
+            this.splitContainerControl3.Panel2.Text = "Panel2";
+            this.splitContainerControl3.Size = new System.Drawing.Size(481, 342);
+            this.splitContainerControl3.SplitterPosition = 271;
+            this.splitContainerControl3.TabIndex = 0;
+            // 
+            // groupControl1
+            // 
+            this.groupControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupControl1.Location = new System.Drawing.Point(0, 0);
+            this.groupControl1.Name = "groupControl1";
+            this.groupControl1.Size = new System.Drawing.Size(271, 342);
+            this.groupControl1.TabIndex = 1;
+            this.groupControl1.Text = "Task";
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.simpleButton1.Location = new System.Drawing.Point(0, 312);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(200, 30);
+            this.simpleButton1.TabIndex = 3;
+            this.simpleButton1.Text = "Send";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textEdit1.Location = new System.Drawing.Point(0, 0);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Consolas", 9F);
+            this.textEdit1.Properties.Appearance.Options.UseFont = true;
+            this.textEdit1.Properties.AutoHeight = false;
+            this.textEdit1.Size = new System.Drawing.Size(200, 342);
+            this.textEdit1.TabIndex = 2;
             // 
             // Form1
             // 
@@ -61,13 +197,14 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(917, 637);
+            this.Controls.Add(this.splitContainerControl1);
             this.Controls.Add(this.radStatusStrip1);
             this.DoubleBuffered = true;
             this.IconOptions.Icon = ((System.Drawing.Icon)(resources.GetObject("Form1.IconOptions.Icon")));
             this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Console";
+            this.Text = "Server Auto Commander";
             this.TopMost = true;
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -75,6 +212,19 @@
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
             ((System.ComponentModel.ISupportInitialize)(this.radStatusStrip1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memoEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl1)).EndInit();
+            this.splitContainerControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl2)).EndInit();
+            this.splitContainerControl2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(splineAreaSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainerControl3)).EndInit();
+            this.splitContainerControl3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.groupControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -84,6 +234,14 @@
         private Telerik.WinControls.UI.RadStatusStrip radStatusStrip1;
         private Telerik.WinControls.UI.RadLabelElement statusText;
         private Telerik.WinControls.Themes.FluentDarkTheme fluentDarkTheme1;
+        private DevExpress.XtraEditors.MemoEdit memoEdit1;
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl1;
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl2;
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private DevExpress.XtraEditors.SplitContainerControl splitContainerControl3;
+        private DevExpress.XtraEditors.GroupControl groupControl1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
     }
 }
 
--- WindowsConsole/Form1.cs
+++ WindowsConsole/Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -21,20 +23,159 @@ namespace WindowsConsole
         private string EnterValue;
         private string ExecuteValue;
 
-        private void LightingShaking()
+        private enum ColorStyle
         {
+            Error,
+            Information,
+            Warning,
+            Question,
+            Complete
+        }
+
+        private void Log(string title, string text, ColorStyle colorStyle)
+        {
+            if (!Directory.Exists(".\\logs"))
+            {
+                Directory.CreateDirectory(".\\logs");
+            }
+            Console.Write("[" + DateTime.Now.ToString() + "]");
+            if (colorStyle == ColorStyle.Error)
+            {
+                Console.Write("[");
+                Console.Write(title, Color.Red);
+                Console.Write("]");
+                Console.WriteLine(text);
+            }
+            else if (colorStyle == ColorStyle.Information)
+            {
+                Console.Write("[");
+                Console.Write(title, Color.White);
+                Console.Write("]");
+                Console.WriteLine(text);
+            }
+            else if (colorStyle == ColorStyle.Warning)
+            {
+                Console.Write("[");
+                Console.Write(title, Color.Yellow);
+                Console.Write("]");
+                Console.WriteLine(text);
+            }
+            else if (colorStyle == ColorStyle.Question)
+            {
+                Console.Write("[");
+                Console.Write(title, Color.SkyBlue);
+                Console.Write("]");
+                Console.WriteLine(text);
+            }
+            else if (colorStyle == ColorStyle.Complete)
+            {
+                Console.Write("[");
+                Console.Write(title, Color.LawnGreen);
+                Console.Write("]");
+                Console.WriteLine(text);
+            }
+            memoEdit1.Text+=("[" + DateTime.Now.ToString() + "]");
+            memoEdit1.Text+=("[");
+            memoEdit1.Text+=(title+"");
+            memoEdit1.Text+=("]");
+            memoEdit1.Text+=(text+"\r\n");
+            streamWriter.Write("[" + DateTime.Now.ToString() + "]");
+            streamWriter.Write("[");
+            streamWriter.Write(title);
+            streamWriter.Write("]");
+            streamWriter.WriteLine(text);
+            streamWriter.Flush();
+        }
+
+        private static StreamWriter streamWriter = new StreamWriter(".\\logs\\" + DateTime.Now.ToFileTime() + ".log");
+
+        private static StreamWriter GameThreadInput;
+        private void Monitor()
+        {
+            PerformanceCounter performanceCounter = new PerformanceCounter("Process", "Working Set", "ServerAutoBash");
+            performanceCounter.NextValue();
             for(; ; )
             {
-                lighting.Visible = false;
-                Thread.Sleep(120);
-                lighting.Visible = true;
-                Thread.Sleep(180);
+                chartControl1.Invoke(new Action(() =>
+                {
+                    chartControl1.Series["Java"].Points.Clear();
+                }));
+                for(int i = 0; i <= 240; i++)
+                {
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+
+                    chartControl1.Series["Java"].Points.AddPoint(i, (double)(performanceCounter.NextValue() / 1024 / 1024));
+
+                    stopwatch.Stop();
+                    if (stopwatch.ElapsedMilliseconds <= 240)
+                    {
+                        Thread.Sleep(int.Parse((250 - stopwatch.ElapsedMilliseconds).ToString().Replace("-", "")));
+                    }
+                }
+            }
+        }
+        private void StartThread()
+        {
+            statusText.Text = "正在启动";
+            Log("Thread - Init", "Pre-Initialzing Application Process ...", ColorStyle.Information);
+            Process process = new Process();
+            Log("Thread - Init", "Pre-Initialzing Application Process Settings ...", ColorStyle.Information);
+            process.StartInfo.FileName = ".\\ServerAutoBash.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            Log("Thread - Init", "Starting Application Process ...", ColorStyle.Information);
+            process.Start();
+            Log("Thread - Init", "Pre-Initialzing Application Process Listener ...", ColorStyle.Information);
+            StreamReader COPor = process.StandardOutput;
+            GameThreadInput = process.StandardInput;
+            Log("Application Thread - Listener Feedback", "Starting Application Process Listener ...", ColorStyle.Information);
+            Thread thread = new Thread(Monitor);
+            thread.Start();
+            statusText.Text = "就绪";
+            for (; ; )
+            {
+                if (process.HasExited == true)
+                {
+                    break;
+                }
+                    try
+                    {
+                        Log("Application Thread - Listener Feedback", COPor.ReadLine(), ColorStyle.Question);
+                    }
+                    catch
+                    {
+
+                    }
+            }
+            try
+            {
+                Log("Application Thread - Stop", "Stopping Application Thread", ColorStyle.Information);
+                Log("Application Thread - Stop", "Stopping Process", ColorStyle.Information);
+                process.Kill();
+                Log("Application Thread - Stop", "Disposing Standard Input Port ...", ColorStyle.Information);
+                GameThreadInput.Dispose();
+                Log("Application Thread - Stop", "Closing Standard Input Port ...", ColorStyle.Information);
+                GameThreadInput.Close();
+                Log("Application Thread - Stop", "Done.", ColorStyle.Complete);
+                Log("Application Thread - Stop", "Disposing Standard Output Port ...", ColorStyle.Information);
+                COPor.Dispose();
+                Log("Application Thread - Stop", "Closing Standard Output Port ...", ColorStyle.Information);
+                COPor.Close();
+                Log("Application Thread - Stop", "Done.", ColorStyle.Complete);
+                Log("Application Thread - Stop", "Everything Was end", ColorStyle.Complete);
+            }
+            catch
+            {
+
             }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            new Thread(LightingShaking).Start();
+            Thread thread = new Thread(StartThread);
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
@@ -60,26 +201,28 @@ namespace WindowsConsole
 
         private void Form1_KeyPress(object sender, KeyPressEventArgs e)
         {
-            if (e.KeyChar == '\n'|| e.KeyChar == '\r')
-            {
-                ExecuteValue = EnterValue;
-                statusText.Text = "就绪";
-            }
-            else if (e.KeyChar == '\b')
-            {
-                EnterValue.Remove(EnterValue.Length - 1);
-                statusText.Text = EnterValue;
-            }
-            else
-            {
-                EnterValue += e.KeyChar;
-                statusText.Text = EnterValue;
-            }
+
         }
 
         private void marqueeProgressBarControl1_EditValueChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (!(textEdit1.Text == ""))
+            {
+                try
+                {
+                    GameThreadInput.WriteLine(textEdit1.Text);
+                    GameThreadInput.Flush();
+                }
+                catch(Exception ex)
+                {
+                    Log("Env - Error", ex.ToString(), ColorStyle.Error);
+                }
+            }
         }
     }
 }
